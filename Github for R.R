ac27c599f9e6f75bc08b0edefec4f6a57803af46
vafasaboori <saboori@gmail.com>
2023-01-05 15:53:19 -0800

    save

--- Github for R.R
+++ Github for R.R
@@ -1,5 +1,34 @@
+# Tutorial: https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r/
+
+# Git is open source software for version control. 
+# Using Git, you can see all previous versions of your code.
+
+# Install Git: https://vimeo.com/511798611
+
+# Configure Git: https://vimeo.com/511798861
+
 install.packages("usethis")
 library(usethis)
 
-edit_git_config()
+edit_git_config() # Enter your name and email
+
+# Initialize a Git Repository: https://vimeo.com/511799486
+# 1st create a new project (check create a git repository)
+# Alternatively
+
+library(usethis)
+use_git()
+
+# View Commit History: https://vimeo.com/511799855
+
+# Make a Commit and View More History: https://vimeo.com/511800674
+
+# Connect RStudio and GitHub
+    # Sign up for GitHub
+    # 
+
+
+
+
+
 
