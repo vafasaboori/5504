42b884b2666b2eb21db71eaf7084fccd92869763
vafasaboori <saboori@gmail.com>
2023-01-04 16:03:39 -0800

    Add viz

--- ggplot_intro.R
+++ ggplot_intro.R
@@ -1,1 +1,14 @@
 library(ggplot2)
+library(palmerpenguins)
+
+# ggplot2 is one of the core members of the tidyverse
+install.packages("tidyverse")
+library(tidyverse)
+
+#mpg data frame found in ggplot2
+library(ggplot2)
+ggplot2::mpg
+
+# To plot mpg, run this code to put displ on the x-axis and hwy on the y-axis:
+ggplot(mpg, aes(x = displ, y = hwy)) + 
+  geom_point()
