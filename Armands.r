ffc596669d5fc3340d69b8af37005dd7814e9492
vafasaboori <saboori@gmail.com>
2023-01-13 18:36:04 -0800

    Chapter 14 Done

--- Armands.r
+++ Armands.r
@@ -11,14 +11,78 @@ cor(armands$Population, armands$Sales)
 
 # Simple linear regression
 armands_slr <- lm(Sales ~ Population, data = armands)
-summary(armands_slr) # coefficients
+armands_slr$coefficients # coefficients
 
 # Plotting data and regression line
 library(tidyverse)
 armands %>% ggplot(aes(x = Population, y = Sales)) + # coordinate system
   geom_point() + # scatter plot
-  geom_smooth(method = 'lm', se = TRUE) # abline with conf interval (gray)
+  geom_smooth(method = 'lm', se = TRUE) # abline with conf. interval (gray)
 
-              
-              
-              
+# R Squared Value
+summary(armands_slr)$r.squared
+
+# Adjusted R Squared Value
+summary(armands_slr)$adj.r.squared
+
+# Complete regression analysis
+summary(armands_slr)
+
+# Confidence and Prediction Interval
+predict (armands_slr, data.frame(Population = 10), interval = "confidence", conf.level = 0.95)
+predict (armands_slr, data.frame(Population = 10), interval = "prediction", conf.level = 0.95)
+
+
+# Residual analysis (method 1)
+armands$predicted <- fitted(armands_slr)
+armands$residuals <- residuals(armands_slr)
+armands$std_residuals <- rstandard(armands_slr)
+
+# Residual plot against x
+armands %>% ggplot(aes(x = Population, y = residuals)) + 
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE) + # we observe no pattern
+  geom_smooth() # we observe no pattern
+
+# Residual plot against y-hat
+armands %>% ggplot(aes(x = predicted, y = residuals)) + 
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE) + # we observe no pattern
+  geom_smooth() # we observe no pattern
+  
+# Standard residual plot against x
+armands %>% ggplot(aes(x = Population, y =std_residuals)) + 
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE) + # we observe no pattern
+  geom_smooth() # we observe no pattern
+
+# QQ Plot (method 1)
+qplot(sample = armands$std_residuals)
+
+# QQ Plot (method 2)
+armands %>% ggplot(aes(sample = std_residuals)) +
+  geom_qq() + 
+  geom_qq_line()
+
+# Calculating Leverage (Hat Values)
+armands$hat <- hatvalues(armands_slr) #none greater than 6/10=0.6
+qplot (armands$Population, armands$hat) +
+  geom_hline(yintercept=0.6, linetype="dashed", color = "red")
+
+# Residual analysis (method 2)
+plot(armands_slr, which = 1) # produces 5 plots
+# 1 = residuals versus fitted
+# 2 = QQ plot
+# 3 = Scale-Location
+# 4 = Cook's Distance
+# 5 = Williams-like Graph
+
+plot(armands_slr, which = 4) #none greater than 4/10=0.4
+armands$cook <- cooks.distance(armands_slr)
+# Cook's distance is a combination of leverage and residual values
+# The higher the leverage and residuals, the higher the Cook’s distance.
+# Investigate any point over 0.5, values over 1.0 are influential
+
+plot(armands_slr, which = 5) #none greater than 4/10=0.4
+# in this plot (leverage ~ std) we are looking for values lying outside dashed line
+
